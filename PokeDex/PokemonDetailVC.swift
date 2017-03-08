1bcbc9f58466497c7d94c6a99bc22e145cbecf55
mikebarone <mikebarone@gmail.com>
2017-03-08 17:47:52 -0500

    Added IBoutlet connection

--- PokeDex/PokemonDetailVC.swift
+++ PokeDex/PokemonDetailVC.swift
@@ -12,11 +12,32 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         nameLbl.text = pokemon.name
+    }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
     }
 
 }
